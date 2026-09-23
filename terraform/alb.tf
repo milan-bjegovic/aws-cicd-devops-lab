2c798cc99ca1d73d1592da8a60b36ac84a4af44b
Milan Bjegovic <oldtimersoffer@gmail.com>
2026-09-23 15:26:39 +0200

    Remove legacy EC2 instance from ALB target group

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -70,12 +70,6 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "app" {
-  target_group_arn = aws_lb_target_group.app.arn
-  target_id        = aws_instance.app.id
-  port             = 80
-}
-
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app.arn
