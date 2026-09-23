934b5467c149e3173c3fdb7ed711701ab7acd5c0
Milan Bjegović <52139507+milan-bjegovic@users.noreply.github.com>
2026-09-23 17:00:27 +0200

    Restrict EC2 HTTP Access to Application Load Balancer
    Restrict application EC2 instances to accept HTTP traffic only from the ALB security group, preventing direct Internet access to port 80 while preserving ALB connectivity.

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -4,11 +4,11 @@ resource "aws_security_group" "app" {
   vpc_id      = "vpc-07da35f64503504ce"
 
   ingress {
-    description = null
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow HTTP only from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
   ingress {
